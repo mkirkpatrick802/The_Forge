f9c894aaf677822cbc6cb2006de0d52ece786cb4
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2024-12-12 18:11:05 -0500

    shader class added

--- The Forge/Engine/Source/Engine/TextureLoader.h
+++ The Forge/Engine/Source/Engine/TextureLoader.h
@@ -26,6 +26,13 @@ namespace Engine
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
         glGenerateMipmap(GL_TEXTURE_2D);
 
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	    glBindTexture(GL_TEXTURE_2D, 0);
+
         stbi_image_free(image);
 
         return textureID;
