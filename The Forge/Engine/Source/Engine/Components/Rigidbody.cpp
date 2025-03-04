830e8e314c06188313fce851b39f7f8d9af21b41
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2025-03-04 00:20:41 -0500

    maybe?

---
+++ The Forge/Engine/Source/Engine/Components/Rigidbody.cpp
@@ -0,0 +1,28 @@
+﻿#include "Rigidbody.h"
+
+Engine::Rigidbody::Rigidbody(): _velocity(), _acceleration(), _mass(1), _inverseMass(0)
+{
+    
+}
+
+void Engine::Rigidbody::OnActivation()
+{
+    _inverseMass = (_mass > 0.0f) ? 1.0f / _mass : 0.0f;
+}
+
+void Engine::Rigidbody::ApplyForce(const glm::vec2 force)
+{
+    _acceleration += force * _inverseMass;
+}
+
+void Engine::Rigidbody::ApplyImpulse(glm::vec2 impulse)
+{
+    _velocity += impulse * _inverseMass;
+}
+
+void Engine::Rigidbody::Update(const float deltaTime)
+{
+    _velocity += _acceleration * deltaTime;
+    gameObject->transform.position += _velocity * deltaTime;
+    _acceleration = glm::vec2(0.0f); // Reset acceleration after integration
+}
---
+++ The Forge/Engine/Source/Engine/Components/Rigidbody.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "ComponentUtils.h"
+
+namespace Engine
+{
+    class Rigidbody final : public Component
+    {
+    public:
+        Rigidbody();
+        void OnActivation() override;
+
+        void Update(float deltaTime) override;
+        void ApplyForce(glm::vec2 force);
+        void ApplyImpulse(glm::vec2 impulse);
+        
+    private:
+        glm::vec2 _velocity;
+        glm::vec2 _acceleration;
+        
+        float _mass;
+        float _inverseMass;
+
+    public:
+        glm::vec2 GetVelocity() const { return _velocity; }
+        float GetInverseMass() const { return _inverseMass; }
+    };
+
+    REGISTER_COMPONENT(Rigidbody)
+}
