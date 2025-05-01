a72e72e30b82e03dfa3065477592c279844a0b82
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2025-04-30 21:23:43 -0400

    f

--- Scripts/Setup-Solution-Windows.bat
+++ Scripts/Setup-Solution-Windows.bat
@@ -2,9 +2,9 @@
 setlocal
 
 set PREMAKE_URL=https://github.com/premake/premake-core/releases/download/v5.0.0-beta5/premake-5.0.0-beta5-windows.zip
-set PREMAKE_DIR=%CD%\..\Premake\Windows
+set PREMAKE_DIR=%CD%\..\Tools\Premake\Windows
 
-PowerShell -ExecutionPolicy Bypass -File %CD%\Utils\Setup-Premake.ps1 "%PREMAKE_URL%" "%PREMAKE_DIR%"
+PowerShell -ExecutionPolicy Bypass -File %CD%\BuildTools\Setup-Premake.ps1 "%PREMAKE_URL%" "%PREMAKE_DIR%"
 
 pushd ..
 %PREMAKE_DIR%\premake5.exe --file=Build.lua vs2022
